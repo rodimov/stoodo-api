245800b916bae438c268d625ed89d265c9193ccb
Artem Rodimov <artyomrodimov@yandex.ru>
2023-02-01 19:41:39 +0200

    Initial commit

---
+++ src/main/java/fr/stoodev/stoodo/StoodoApplication.java
@@ -0,0 +1,13 @@
+package fr.stoodev.stoodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoodoApplication.class, args);
+	}
+
+}
