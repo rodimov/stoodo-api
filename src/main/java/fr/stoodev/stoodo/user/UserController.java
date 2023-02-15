05d35aca6b2a4bc666e057a5f3bc11814a2cce0c
Artem Rodimov <artyomrodimov@yandex.ru>
2023-02-15 23:08:20 +0200

    feat: update api path

--- src/main/java/fr/stoodev/stoodo/user/UserController.java
+++ src/main/java/fr/stoodev/stoodo/user/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/user")
 public class UserController {
     private final UserServiceImpl userServiceImpl;
 
