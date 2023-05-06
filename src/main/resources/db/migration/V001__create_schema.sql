36782a99cc512131e9825773cb7ab5afd3b25a34
Artem Rodimov <artyomrodimov@yandex.ru>
2023-05-06 23:29:57 +0300

    feat: added migration with posts

--- src/main/resources/db/migration/V001__create_schema.sql
+++ src/main/resources/db/migration/V001__create_schema.sql
@@ -10,18 +10,58 @@ CREATE TABLE IF NOT EXISTS users (
     is_locked BOOLEAN,
     is_credentials_valid BOOLEAN,
     is_active BOOLEAN,
-    created_by BIGINT,
+    created_by BIGINT REFERENCES users,
     created_at TIMESTAMP,
-    last_modified_by BIGINT,
+    last_modified_by BIGINT REFERENCES users,
     last_modified_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS posts (
     id SERIAL PRIMARY KEY,
     title VARCHAR(256),
-    slug VARCHAR(256),
-    description VARCHAR(256),
-    owner_id BIGINT REFERENCES users
+    slug VARCHAR(256) UNIQUE NOT NULL,
+    image_url VARCHAR(1024),
+    description VARCHAR(1024),
+    owner_id BIGINT REFERENCES users,
+    is_published BOOLEAN,
+    created_by BIGINT REFERENCES users,
+    created_at TIMESTAMP,
+    last_modified_by BIGINT REFERENCES users,
+    last_modified_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS posts_content (
+    id SERIAL PRIMARY KEY,
+    text TEXT NOT NULL,
+    post_id BIGINT REFERENCES posts,
+    previous_version BIGINT REFERENCES posts_content,
+    next_version BIGINT REFERENCES posts_content,
+    created_by BIGINT REFERENCES users,
+    created_at TIMESTAMP,
+    last_modified_by BIGINT REFERENCES users,
+    last_modified_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+    id SERIAL PRIMARY KEY,
+    tag VARCHAR(256) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tags_posts (
+    id SERIAL PRIMARY KEY,
+    tag_id BIGINT REFERENCES tags,
+    post_id BIGINT REFERENCES posts
+);
+
+CREATE TABLE IF NOT EXISTS topics (
+    id SERIAL PRIMARY KEY,
+    topic VARCHAR(256) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS topics_posts (
+    id SERIAL PRIMARY KEY,
+    topic_id BIGINT REFERENCES topics,
+    post_id BIGINT REFERENCES posts
 );
 
 CREATE TABLE IF NOT EXISTS refresh_tokens (
